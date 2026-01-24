9f5065c45cd426f086c90094f7ab8d12dd4ced2f
Pasidu2010 <madushankapasiya0@gmail.com>
2026-01-24 08:18:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Q2kTRQST#xAcFYJ8vstZYqWa-WiXvS7acnzCGYh8J_ZNGRlG0XnI",
+SESSION_ID: process.env.SESSION_ID || "pykx3Q7a#bjcfYhKcVsiUNr32U6_GHLDyNi1uuP37gwQeAwZGhDo",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/su67w9.jpg", 
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello 마두샨카bot alive",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
